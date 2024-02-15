f71b19ce0bc3d5ce2557f93bb1ca2502980af6b1
naila95 <naila.ahmedli1995@gmail.com>
2024-02-15 12:49:48 +0400

    initial

---
+++ src/app/comments/data.ts
@@ -0,0 +1,5 @@
+export const comments = [
+  { id: 1, comment: "first comment" },
+  { id: 2, comment: "second comment" },
+  { id: 3, comment: "third comment" },
+];
---
+++ src/app/comments/route.ts
@@ -0,0 +1,20 @@
+import { comments } from "./data";
+
+export async function GET() {
+  return Response.json(comments);
+}
+
+export async function POST(request: Request) {
+  const comment = await request.json();
+  const newComment = {
+    id: comments.length + 1,
+    comment: comment.text,
+  };
+  comments.push(newComment);
+  return new Response(JSON.stringify(newComment), {
+    headers: {
+      "Content-type": "application/json",
+    },
+    status: 201,
+  });
+}
